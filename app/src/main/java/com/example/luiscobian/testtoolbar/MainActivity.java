13dd803a25ded3fc051c54b3d6390e799373c14c
Luis Cobian <coby10@gmail.com>
2017-05-07 23:12:21 -0500

    Ejemplo de un ToolBar personalizado cambiando iconos y agregando Menus

---
+++ app/src/main/java/com/example/luiscobian/testtoolbar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.luiscobian.testtoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mi_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.create_new2:
+                Toast.makeText(this,"Esta es una prueba",Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
